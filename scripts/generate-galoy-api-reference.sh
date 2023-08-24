0d2798c83cd389248443a48e5b9e28a6b324141b
openoms <oms@tuta.io>
2023-08-24 18:21:31 +0200

    chore: make the api references dark

--- scripts/generate-galoy-api-reference.sh
+++ scripts/generate-galoy-api-reference.sh
@@ -3,9 +3,8 @@
 # deps
 if ! git --version; then sudo apt-get install -y git; fi
 if ! node --version; then sudo apt-get install -y nodejs; fi
-if ! yarn --version; then sudo apt-get install -y yarn ; fi
+if ! yarn --version; then sudo apt-get install -y yarn; fi
 if ! npx spectaql --version; then yarn add spectaql --non-interactive; fi
-
 
 mkdir -p .temp
 cd .temp || exit 1
@@ -23,3 +22,7 @@ npx spectaql ./../../scripts/spectaql/spectaql-config-public-api.yml \
 # build admin api reference
 npx spectaql ./../../scripts/spectaql/spectaql-config-admin-api.yml \
   -t ./../../static -f admin-api-reference.html || exit 1
+
+# set dark mode
+sed -i 's/spectaql.min.css/spectaql.dark.css/' ./../../static/public-api-reference.html
+sed -i 's/spectaql.min.css/spectaql.dark.css/' ./../../static/admin-api-reference.html

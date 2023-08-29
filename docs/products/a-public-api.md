a719f67b60aeb92af7f9728e95d5cdb69aeb5eaa
openoms <oms@tuta.io>
2023-08-29 15:37:29 +0200

    chore: add fee probe and pay invoice

--- docs/products/a-public-api.md
+++ docs/products/a-public-api.md
@@ -11,19 +11,20 @@ import AuthRequestButton from '/src/components/graphql/AuthRequestButton';
 
 # Public API
 
+:::note
+The calls are directed to the staging environment by default where the funds are worthless, using the default bitcoin [signet](/signet.md).
+:::
+
 * Find the staging API endpoint and GraphQL playground at:
 
     https://api.staging.galoy.io/graphql
 
-## Galoy API Tutorial
-
-### Overview
-
-The staging API endpoint is: `https://api.staging.galoy.io/graphql`
-
 ### Authentication
 
-To get a new auth token:
+To get started :
+* [Download Blink](/products#download-the-blink-app)
+* [Connect to the staging environment](/products#connect-to-the-staging-environment)
+* [Set up the email login](http://localhost:3000/products#set-up-the-email-login)
 
 ## Email Login
 
@@ -38,59 +39,13 @@ To get a new auth token:
   <AuthRequestButton />
 </AuthProvider>
 
-
-#### lnInvoiceFeeProbe <a href="#lninvoicefeeprobe" id="lninvoicefeeprobe"></a>
-
-**query**
-
-```
-export PAYMENT_REQUEST='lntb123450n1pscxulepp59x872uskmr0a4r3wnr3vkmr297zm53smw8j4efevpknuwm8vs5yqdq5w35hqurfdenjq6nsdamscqzpuxqyz5vqsp5yzmznk5z7xszkgfk5xstuh8j5gg4srerelv58pph5wjan2kd8rqs9qyyssqe5l376x893374kqsr5lc8tesudg4jryaqlzmx44mfr87nds83margfa09ggd92sy0rudl6r79sat4rxqml5yfdhmm7yk9jc0ugzw7hgpdfxfzk'
-curl --location --request POST $URI --header "$AUTH_TOKEN" --header 'Content-Type: application/json' --data-raw '{"query":"mutation lnInvoiceFeeProbe ($input: LnInvoiceFeeProbeInput!) {\n    lnInvoiceFeeProbe (input: $input) {\n        errors {\n            message\n            path\n        }\n        amount\n    }\n}","variables":{"input":{"paymentRequest":"'"$PAYMENT_REQUEST"'"}}}'
-```
-
-**response**
-
-```
-{
-    "data": {
-        "lnInvoiceFeeProbe": {
-            "errors": [],
-            "amount": 0
-        }
-    }
-}
-```
-
-#### lnInvoicePaymentSend <a href="#lninvoicepaymentsend" id="lninvoicepaymentsend"></a>
-
-**query**
-
-```
-PAYMENT_REQUEST='lntb50u1psc8xeppp5yryt4j8ve0pvyunrp3dr0l5tj0ss4hnkuxz52hldc9nz2njpe2fqdqqxqrrss9qy9qsqsp5xduj0a8u2c7mn959045wnu8xrv63vqye2d2zw3kllahdv47np6qsrzjqwfn3p9278ttzzpe0e00uhyxhned3j5d9acqak5emwfpflp8z2cng85uzyqqqdsqqqqqqqlgqqqqqeqqjqjwcuu47akd6qggafxy403zk9f67xh86huv6r0fkngvkuwrxuadv8dd3u5gfeurshh3q6jr5jvu05z7f63xz5ac8v4pk3tjfv46jt5dcprr9d4z'
-curl --location --request POST $URI --header "$AUTH_TOKEN" --header 'Content-Type: application/json' --data-raw '{"query":"mutation lnInvoicePaymentSend ($input: LnInvoicePaymentInput!) {\n    lnInvoicePaymentSend (input: $input) {\n        errors {\n            message\n            path\n        }\n        status\n    }\n}","variables":{"input":{"paymentRequest":"'"$PAYMENT_REQUEST"'","memo":"'"$MEMO"'"}}}'
-```
-
-**response**
-
-```
-{
-    "data": {
-        "lnInvoicePaymentSend": {
-            "errors": [],
-            "status": "ALREADY_PAID"
-        }
-    }
-}
-```
-
 ## [Postman collection](https://github.com/GaloyMoney/galoy/tree/main/docs/postman-collection)
-If you use Postman, we have a collection you can import to test the API.
-
-Download it here: [Lightning Integration.postman\_collection.json](https://github.com/GaloyMoney/galoy/blob/main/docs/postman-collection/galoy_graphql_main_api.postman_collection.json)
+* If you use Postman, we have a collection you can import to test the API.
+* Download it here: [Lightning Integration.postman\_collection.json](https://github.com/GaloyMoney/galoy/blob/main/docs/postman-collection/galoy_graphql_main_api.postman_collection.json)
 
 ## [Hoppscotch.io collection](https://hoppscotch.io/graphql)
-* connect to the staging environment: https://api.staging.galoy.io/graphql
-* import the collection from a [file](/hoppscotch-collection.json) or [gist](https://gist.githubusercontent.com/openoms/e365104e5b459bd6cead7df0e5c24ff3/raw/fa671f639649accd72304381d0679be610a1b377/hoppscotch-collections.json)
+* Connect to the staging environment: https://api.staging.galoy.io/graphql
+* Import the collection from a [file](/hoppscotch-collection.json) or [gist](https://gist.githubusercontent.com/openoms/e365104e5b459bd6cead7df0e5c24ff3/raw/fa671f639649accd72304381d0679be610a1b377/hoppscotch-collections.json)
 
 ## Videos
 ### Using the Galoy GraphQL API
--- src/components/graphql/AuthRequestButton.jsx
+++ src/components/graphql/AuthRequestButton.jsx
@@ -18,8 +18,17 @@ function AuthRequestButton() {
   const [walletData, setWalletData] = useState(null);   // <-- for the wallet query
   const [invoiceData, setInvoiceData] = useState(null); // <-- for the invoice query
 
+  const [paymentRequest, setPaymentRequest] = useState('');
+  const [errorMessageFetchFeeProbe, setErrorMessageFetchFeeProbe] = useState(null);
+  const [curlCommandFeeProbe, setCurlCommandFeeProbe] = useState('');
+  const [feeProbeData, setFeeProbeData] = useState(null);
+
+  const [lnInvoicePaymentData, setLnInvoicePaymentData] = useState(null);
+  const [errorMessageLnInvoicePayment, setErrorMessageLnInvoicePayment] = useState(null);
+  const [curlCommandLnInvoicePayment, setCurlCommandLnInvoicePayment] = useState('');
+
   const getWalletQuery = `
-    query Me { me { defaultAccount { ... on ConsumerAccount { defaultWalletId }}}}`;
+query Me { me { defaultAccount { ... on ConsumerAccount { defaultWalletId }}}}`;
 
   const getInvoiceQuery = (amount, memo, walletId) => `
 mutation LnInvoiceCreate($input: LnInvoiceCreateInput!) {\
@@ -35,34 +44,68 @@ mutation LnInvoiceCreate($input: LnInvoiceCreateInput!) {\
   satoshis\
   }}}`;
 
-  const generateCurlCommand = (query, type) => {
-    const requestBody = {
+  const getFeeProbeQuery = (paymentRequest, walletId) => `
+  mutation lnInvoiceFeeProbe($input: LnInvoiceFeeProbeInput!) {\
+    lnInvoiceFeeProbe(input: $input) {\
+    errors {\
+    message\
+    }\
+    amount\
+    }}`;
+
+  const getInvoiceSendQuery = (paymentRequest, walletId) => `
+  mutation LnInvoicePaymentSend($input: LnInvoicePaymentInput!) {\
+    lnInvoicePaymentSend(input: $input) {\
+    status\
+    errors {\
+    message\
+    path\
+    code\
+    }}}`;
+
+  const generateCurlCommand = (query, type, paymentRequest = '', walletId = '') => {
+    let requestBody = {
       query: query.trim(),
       variables: {}
     };
+
+    const authHeader = authToken
+      ? `--header 'Authorization: Bearer ${authToken}'`
+      : "--header 'Authorization: Bearer <YOUR_AUTH_TOKEN_HERE>'";
+
     if (type === 'invoice') {
       requestBody.variables.input = {
         amount: amount.toString(),
         memo: memo,
         walletId: defaultWalletId,
       };
+    } else if (type === 'feeProbe') {
+      requestBody.variables.input = {
+        paymentRequest: paymentRequest,
+        walletId: defaultWalletId,   // You should use walletId argument passed to the function
+      };
+    } else if (type === 'lnInvoicePaymentSend') {
+      requestBody.variables.input = {
+        paymentRequest: paymentRequest,
+        walletId: walletId
+      };
     }
-    const queryData = JSON.stringify(requestBody).replace(/\n/g, '');
 
-    // Choose the Authorization header based on authToken availability
-    const authHeader = authToken
-      ? `--header 'Authorization: Bearer ${authToken}'`
-      : "--header 'Authorization: Bearer <YOUR_AUTH_TOKEN_HERE>'";
+    let queryData = JSON.stringify(requestBody).replace(/\n/g, '');
 
     const command = `curl --request POST --header 'content-type: application/json' \\
-  ${authHeader} \\
-  --url '${apiEndpoint}' \\
-  --data '${queryData}'`;
+    ${authHeader} \\
+    --url '${apiEndpoint}' \\
+    --data '${queryData}'`;
 
     if (type === 'wallet') {
       setCurlCommandWallet(command);
     } else if (type === 'invoice') {
       setCurlCommandInvoice(command);
+    } else if (type === 'feeProbe') {
+      setCurlCommandFeeProbe(command);
+    } else if (type === 'lnInvoicePaymentSend') {
+      setCurlCommandLnInvoicePayment(command);
     }
   };
 
@@ -98,6 +141,41 @@ mutation LnInvoiceCreate($input: LnInvoiceCreateInput!) {\
     }
   };
 
+  const fetchLnInvoicePaymentData = async () => {
+    const query = getInvoiceSendQuery(paymentRequest, defaultWalletId);
+    const variables = {
+      input: {
+        paymentRequest: paymentRequest,
+        walletId: defaultWalletId
+      }
+    };
+
+    try {
+      const data = await handleAuthenticatedRequest(authToken, apiEndpoint, query, variables);
+      setLnInvoicePaymentData(data);
+      generateCurlCommand(query, 'lnInvoicePaymentSend', paymentRequest, defaultWalletId);
+    } catch (error) {
+      setErrorMessageLnInvoicePayment(error.message);
+    }
+  };
+
+  const fetchFeeProbeData = async () => {
+    const query = getFeeProbeQuery(paymentRequest, defaultWalletId);
+    const variables = {
+      input: {
+        paymentRequest: paymentRequest,
+        walletId: defaultWalletId,
+      }
+    };
+
+    try {
+      const data = await handleAuthenticatedRequest(authToken, apiEndpoint, query, variables);
+      setFeeProbeData(data);  // <-- Set the feeProbeData with the response
+      generateCurlCommand(query, 'feeProbe', paymentRequest, defaultWalletId);
+    } catch (error) {
+      setErrorMessageFetchFeeProbe(error.message);
+    }
+  };
 
   useEffect(() => {
     // This will be triggered whenever authToken or apiEndpoint changes
@@ -110,19 +188,16 @@ mutation LnInvoiceCreate($input: LnInvoiceCreateInput!) {\
     generateCurlCommand(query, 'invoice');
   }, [authToken, apiEndpoint, amount, memo, defaultWalletId]); // Listening to these states changes
 
-  function AuthTokenInput({ value, onChange, onSet }) {
-    return (
-      <div>
-        <input
-          type="text"
-          placeholder="Enter authToken"
-          value={value}
-          onChange={onChange}
-        />
-        <button onClick={onSet}>Set Token</button>
-      </div>
-    );
-  }
+  useEffect(() => {
+    // This will be triggered whenever authToken, apiEndpoint, paymentRequest, or defaultWalletId changes
+    const query = getFeeProbeQuery(paymentRequest, defaultWalletId);
+    generateCurlCommand(query, 'feeProbe', paymentRequest, defaultWalletId);
+  }, [authToken, apiEndpoint, paymentRequest, defaultWalletId]);
+
+  useEffect(() => {
+    const query = getInvoiceSendQuery(paymentRequest, defaultWalletId);
+    generateCurlCommand(query, 'lnInvoicePaymentSend', paymentRequest, defaultWalletId);
+  }, [authToken, apiEndpoint, paymentRequest, defaultWalletId]);
 
   const handleAuthTokenChange = (e) => {
     setManualAuthToken(e.target.value);
@@ -147,6 +222,20 @@ mutation LnInvoiceCreate($input: LnInvoiceCreateInput!) {\
   const handleDefaultWalletIdChange = (e) => {
     setDefaultWalletId(e.target.value);
   };
+
+  function AuthTokenInput({ value, onChange, onSet }) {
+    return (
+      <div>
+        <input
+          type="text"
+          placeholder="Enter authToken"
+          value={value}
+          onChange={onChange}
+        />
+        <button onClick={onSet}>Set Token</button>
+      </div>
+    );
+  }
 
   const authTokenSection = (
     <AuthTokenInput
@@ -235,6 +324,87 @@ mutation LnInvoiceCreate($input: LnInvoiceCreateInput!) {\
           {curlCommandInvoice}
         </pre>
       </div>
+
+      {/* Display for FeeProbe */}
+      <h2>Probe invoice fee</h2>
+      <div>
+        <label>
+          Payment Request:
+          <input
+            type="text"
+            value={paymentRequest}
+            onChange={e => setPaymentRequest(e.target.value)}
+            style={{ marginLeft: '10px', marginRight: '20px' }}
+          />
+        </label>
+        <label>
+          Wallet ID:
+          <input
+            type="text"
+            value={defaultWalletId}
+            onChange={handleDefaultWalletIdChange}
+            style={{ marginLeft: '10px' }}
+          />
+        </label>
+      </div>
+      <button onClick={fetchFeeProbeData}>Probe Fee</button>
+      {errorMessageFetchFeeProbe && <div style={{ color: 'red' }}>Error: {errorMessageFetchFeeProbe}</div>}
+      {feeProbeData && <div><strong>Fee Probe Data:</strong> <pre>{JSON.stringify(feeProbeData, null, 2)}</pre></div>}
+
+      <div style={{ marginTop: '20px' }}>
+        <h3>cURL command to probe invoice fee:</h3>
+        <pre style={{
+          backgroundColor: 'auto',
+          padding: '10px',
+          border: '1px solid #ccc',
+          borderRadius: '5px',
+          overflowX: 'auto',
+          whiteSpace: 'pre-wrap'
+        }}>
+          {curlCommandFeeProbe}
+        </pre>
+      </div>
+
+      {/* Display for invoiceSend */}
+      <h2>Pay an invoice</h2>
+      <div>
+        <label>
+          Payment Request:
+          <input
+            type="text"
+            value={paymentRequest}
+            onChange={e => setPaymentRequest(e.target.value)}
+            style={{ marginLeft: '10px', marginRight: '20px' }}
+          />
+        </label>
+        <label>
+          Wallet ID:
+          <input
+            type="text"
+            value={defaultWalletId}
+            onChange={handleDefaultWalletIdChange}
+            style={{ marginLeft: '10px' }}
+          />
+        </label>
+      </div>
+      <button onClick={fetchLnInvoicePaymentData}>Send Payment</button>
+      {errorMessageLnInvoicePayment && <div style={{ color: 'red' }}>Error: {errorMessageLnInvoicePayment}</div>}
+      {lnInvoicePaymentData && <div><strong>Payment Data:</strong> <pre>{JSON.stringify(lnInvoicePaymentData, null, 2)}</pre></div>}
+
+      <div style={{ marginTop: '20px' }}>
+        <h3>cURL command to pay an invoice:</h3>
+        <pre style={{
+          backgroundColor: 'auto',
+          padding: '10px',
+          border: '1px solid #ccc',
+          borderRadius: '5px',
+          overflowX: 'auto',
+          whiteSpace: 'pre-wrap'
+        }}>
+          {curlCommandLnInvoicePayment}
+        </pre>
+      </div>
+
     </div>
   );
 }
